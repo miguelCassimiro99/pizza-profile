1c0c7ec879c48024f3cf924b0b50643c2df775f1
Miguel Cassimiro <miguels@umdoisesportes.com.br>
2023-01-06 20:55:32 -0300

    chore: first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="snap-mandatory snap-center scrollbar scrollbar-thumb-red-900 scrollbar-track-gray-800">
+    <h1 class="text-2xl font-bold h-screen">Miguel Cassimiro</h1>
+    <div class="h-screen">
+      <span>second layer</span>
+    </div>
+  </div>
+</template>
