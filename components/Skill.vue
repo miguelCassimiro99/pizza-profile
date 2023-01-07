62532abcac2052a1df80525af6e43be5e7ea738a
Miguel Cassimiro <miguels@umdoisesportes.com.br>
2023-01-06 23:00:49 -0300

    fix: skills hover circle

--- components/Skill.vue
+++ components/Skill.vue
@@ -10,12 +10,12 @@ defineProps<ISkill>()
 <template lang="pug">
 div(class="group relative flex cursor-pointer")
   img(
-    class="w-14 h-14 rounded-full border object-cover xl:h-12 xl:w-12 filter group-hover:grayscale transition duration-300 ease-in-out"
+    class="w-14 h-14 rounded-full border object-cover xl:h-16 xl:w-16 filter group-hover:grayscale transition duration-300 ease-in-out"
     :src="logoUrl"
     :alt="name"
   )
   div(
-    class="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-10 w-10 xl:h-12 xl:w-12 rounded-full z-0"
+    class="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-14 w-14 xl:h-16 xl:w-16 rounded-full z-0"
   )
     div(class="flex items-center justify-center h-full")
       p(class="text-base font-bol text-black opacity-100 p-2") {{ level || "100%" }}
