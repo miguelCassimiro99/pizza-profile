4fac1ef02312f033c5f9a5eb25b4ef69d94d336f
Miguel Cassimiro <miguels@umdoisesportes.com.br>
2023-01-18 09:46:33 -0300

    feat: adding new layout page

---
+++ pages/retrowave.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+h2(class="") Hello world
+AtomsSectionTitleComponent(:label="projectName")
+</template>
+<script lang="ts" setup>
+const projectName = "Miguel Projeto"
+
+</script>
